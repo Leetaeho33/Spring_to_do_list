db71c53fa480b7b531c64144be02ec37fd24a949
taeholee <lth1993@naver.com>
2023-11-13 20:41:04 +0900

    UserService signUp()구현

--- src/main/java/com/example/to_do_list/service/UserService.java
+++ src/main/java/com/example/to_do_list/service/UserService.java
@@ -1,4 +1,25 @@
 package com.example.to_do_list.service;
 
+import com.example.to_do_list.dto.UserRequestDto;
+import com.example.to_do_list.entity.User;
+import com.example.to_do_list.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    public void signUp(UserRequestDto userRequestDto){
+        User user = new User(userRequestDto);
+        userRepository.save(user);
+    }
+    public void logIn(UserRequestDto userRequestDto){
+
+    }
 }
